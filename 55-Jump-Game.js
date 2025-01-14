597653627a6852f49ff5bcd3d893f9dbf133575f
Abdiladif Gurhan <134311440+AbdiladifG@users.noreply.github.com>
2025-01-13 19:02:50 -0500

    [55] [Time Beats: 87.56%] [Memory Beats: 88.36%] - LeetPush

--- 55-Jump-Game.js
+++ 55-Jump-Game.js
@@ -3,11 +3,12 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    let last = nums.length - 1; // Start at the last index
-    for (let i = nums.length - 2; i >= 0; i--) {
-        if (nums[i] >= last - i) {
-            last = i; // Update `last` to the current index if it can jump to or beyond the current `last`
+    let g = nums.length - 1
+    for(let i = nums.length-2; i >= 0; i--){
+        if(i + nums[i] >= g){
+            g = i
         }
     }
-    return last === 0; // Check if `last` moved all the way to the start
+
+    return g == 0
 };
